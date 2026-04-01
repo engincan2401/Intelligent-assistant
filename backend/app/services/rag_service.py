daca31bca0179c8fe17843e0da5c077e721bcb90
Engindzhan Berber <74961836+engincan2401@users.noreply.github.com>
2026-04-01 22:06:59 +0300

    оптимизация на RAG с MMR, CUDA поддръжка и изчистване на зависимостите

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -1,9 +1,9 @@
-from langchain_community.llms import Ollama
+from langchain_ollama import ChatOllama
 from langchain_chroma import Chroma
 from langchain_core.prompts import PromptTemplate
 from app.services.vector_service import embedding_model, CHROMA_PATH
 
-llm = Ollama(model="llama3", temperature=0.1)
+llm = ChatOllama(model="llama3", temperature=0.1)
 
 PROMPT_TEMPLATE = """
 Ти си полезен AI асистент. Използвай САМО следния контекст, за да отговориш на въпроса. 
@@ -29,13 +29,21 @@ def generate_answer(question: str):
         collection_name="diploma_documents"
     )
 
-    retriever = db.as_retriever(search_kwargs={"k": 3})
+    # Оптимизация с MMR (Maximal Marginal Relevance)
+    retriever = db.as_retriever(
+        search_type="mmr", 
+        search_kwargs={
+            "k": 3,              # Брой финални резултати, които ще подадем на LLM-а
+            "fetch_k": 10,       # Изтегля първо 10 релевантни резултата от базата
+            "lambda_mult": 0.5   # Балансира (0 = пълно разнообразие, 1 = максимална релевантност)
+        }
+    )
     docs = retriever.invoke(question)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc in docs])
 
-
     final_prompt = prompt.format(context=context_text, question=question)
     response_text = llm.invoke(final_prompt)
 
-    return response_text, docs
+    # Връщаме само текста (.content), защото ChatOllama връща AIMessage обект
+    return response_text.content, docs
--- backend/app/services/vector_service.py
+++ backend/app/services/vector_service.py
@@ -1,13 +1,24 @@
 import os
+import torch
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 CHROMA_PATH = "data/chroma_db"
 os.makedirs(CHROMA_PATH, exist_ok=True)
 
+
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"  
+else:
+    device = "cpu"  
+
+print(f"Инициализиране на модела за векторизиране на устройство: {device.upper()}")
+
 embedding_model = HuggingFaceEmbeddings(
     model_name="BAAI/bge-m3",
-    model_kwargs={'device': 'cpu'},
+    model_kwargs={'device': device},
     encode_kwargs={'normalize_embeddings': True}
 )
 
@@ -15,12 +26,11 @@ def add_chunks_to_vector_db(chunks):
     if not chunks:
         return False
     
-
     db = Chroma(
         persist_directory=CHROMA_PATH,
         embedding_function=embedding_model,
         collection_name="diploma_documents"
-        )
+    )
 
     db.add_documents(chunks)
 
